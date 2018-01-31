2034cd5dcd08820ace2fed295e3286a82f76b8bc
PrarabdhJoshi <prarabdh@ufl.edu>
2018-01-31 00:04:20 -0500

    added user command with an alias

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var fetch_data = require('./dependency.js');
-fetch_data('AutoCustoms');
+
 
 //get instance of the commander
 var cli = require('commander');
@@ -8,5 +8,14 @@ var cli = require('commander');
 cli
     .version('1.0.0')
     .description('GitHub Info Collecter')
+
+//add commands 
+cli
+    .command('user <username>')
+    .alias('u')
+    .description('get public repositories for a given user')
+    .action((username)=>{
+        fetch_data(username);
+    });
 
 cli.parse(process.argv);
