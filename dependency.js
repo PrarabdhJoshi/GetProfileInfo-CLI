c064d381038cf472238f7a6ef9e98c2161402a2a
PrarabdhJoshi <prarabdh@ufl.edu>
2018-01-30 22:42:28 -0500

    function written to fetch dynamic requests - fixes #3

--- dependency.js
+++ dependency.js
@@ -4,15 +4,20 @@ var headers = {
     'User-Agent': 'PrarabdhJoshi'
 };
 
-var options = {
-    url: 'https://api.github.com/users/AutoCustoms/repos',
-    headers: headers
-};
 
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-        console.log(JSON.parse(body).length);
-    }
+
+
+function get_data(username){
+    request({
+        url: 'https://api.github.com/users/'+username+'/repos',
+        headers: headers
+        }, (error, response, body)=>{
+        if (!error && response.statusCode == 200) {
+            console.log(JSON.parse(body).length);
+    
+        }
+    });
+    
 }
 
-request(options, callback);
+get_data('AutoCustoms');
